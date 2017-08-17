e04db1aa8b6cc4d65ea29a12e5565a06b1181c9e
Maegan Jean Avant <mavant@basecampcodingacademy.org>
2017-08-17 11:17:18 -0500

    modified

--- shell.py
+++ shell.py
@@ -1,21 +1,22 @@
 import core, time, sys
 from random import randint
 
-# def slow_type(th):
-#     typing_speed = 1
-#     for l in t:
-#         sys.stdout.write(l)
-#         sys.stdout.flush()
-#         time.sleep(typing_speed / 970.0)
-#     return input()
+
+def slow_type(t):
+    typing_speed = 10
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(typing_speed / 970.0)
+    return input()
 
 
 def get_move():
     ''' gets the user decision for which move to do '''
     while True:
-        decision = input(
+        decision = slow_type(
             '\n Whats your move fighter?\n -Attack\n -Heal\n').strip().lower()
-        if decision in ['attack', 'heal']:
+        if decision in ['attack', 'heal\n']:
             return decision
         print('Invalid choice!')
 
@@ -33,7 +34,8 @@ def main():
     gladiator_2 = core.new_gladiator('Player 2', health, rage, damage_low,
                                      damage_high)
     while True:
-        print('*************************************************************')
+        print(
+            '\n*************************************************************')
         print('{}: Health:{}, Rage:{}, \n{}: Health:{}, Rage:{}'.format(
             gladiator_1['Name'], gladiator_1['Health'], gladiator_1['Rage'],
             gladiator_2['Name'], gladiator_2['Health'], gladiator_2['Rage']))
@@ -54,7 +56,8 @@ def main():
             exit()
 
 ######################################PLAYER 2##################################
-        print('*************************************************************')
+        print(
+            '\n*************************************************************')
         print('{}: Health:{}, Rage:{}, \n{}: Health:{}, Rage:{}'.format(
             gladiator_1['Name'], gladiator_1['Health'], gladiator_1['Rage'],
             gladiator_2['Name'], gladiator_2['Health'], gladiator_2['Rage']))
@@ -71,7 +74,7 @@ def main():
             core.heal(gladiator_2)
 
         if core.is_dead(gladiator_1):
-            print('\nGame Over. Gladiator 2 wins!')
+            print('\nGame Over ~_~ Gladiator 2 wins!')
             exit()
 
 if __name__ == '__main__':
---
+++ test_core.py
@@ -0,0 +1,18 @@
+import core, shell
+
+
+def test_decision():
+    """
+    Tests if decision (attack or heal) returns
+    health and rage status.
+    """
+    assert core.attack(attacker, defender)
+
+
+# def test_is_dead():
+
+# def test_heal():
+
+# def test_health():
+#     assert
+#     True
